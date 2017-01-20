5c3812876da19f80be39f5ff732480e3ef6f1081
twisty-doerofthings <twisty.doerofthings@gmail.com>
2017-01-20 09:10:41 -0800

    Create header.php

---
+++ inc/header.php
@@ -0,0 +1,11 @@
+<head>
+        <title><?php echo $title; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="style/jquery.mobile-1.4.5.min.css" />
+        <link rel="stylesheet" href="style/dark_hive/jquery-ui.css" />
+        <link rel="stylesheet" href="style/style.css" />
+        <script src="jquery/jquery-1.12.4.min.js"></script>
+        <script src="jquery/jquery.mobile-1.4.5.min.js"></script>
+        <script src="jquery/jquery-ui-1.12.1.min.js"></script>
+        <script src="jquery/scripts.js"></script>
+</head>
