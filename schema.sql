e0e9f984555f84d3b49d88f8318df0ad0b77bbcf
smujushi <github@mujushi.com>
2017-01-23 20:29:18 -0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS `beta_player` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `now` date NOT NULL,
+  `player_id` int(11) NOT NULL,
+  `score` varchar(32) DEFAULT NULL,
+  `num` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+)
+
 CREATE TABLE IF NOT EXISTS `beta_rounds` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `open` int(11) NOT NULL,
